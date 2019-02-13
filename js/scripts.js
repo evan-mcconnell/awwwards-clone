1449b761a8ab4f20f4752181e5e6c1f0a9dd5d12
Dino Nguyen and Kimberly  McConnell <dino@>
2019-02-13 15:54:38 -0800

    create awwwards clone with responsive design using media queries and sass

---
+++ js/scripts.js
@@ -0,0 +1,5 @@
+$(function(){
+  for (i = 0; i < 20; i++) {
+    $(".sites").append('<div class="web-card"><img src="img/moves.jpg" alt="moves website preview"><div class="card-info"><p>Basic® - Moves</p><p>From United States</p><p>February 13, 2019</p></div><div class="card-item"><a href="#"><img src="img/basic.jpg" alt=""></a><a href="#">By Basic</a><span class="item">HM</span><span class="item">MW</span><span class="item">DEV</span><span class="item">SOTD</span></div></div>')
+  }
+});
